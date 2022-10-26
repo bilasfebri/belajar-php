f99d98f4f4da513341c87a094c0bd2d13185a557
Bilas Febri Dwi Ananda <bilasbwi@gmail.com>
2022-10-26 11:16:45 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Bilas Febri";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Dwi Ananda";
+    $umurAdik = 7;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
